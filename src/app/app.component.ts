6a6bc4bbf0a0528ca072f1542ce70307019251a0
nir@500tech.com <nir@500tech.com>
2016-12-01 11:07:55 +0200

    basic list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
+import { AngularCoreTrainingPage } from './../../e2e/app.po';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>ANGULAR CORE TRAINING</h1>`,
+  template: `
+      <h1>Todolist</h1>
+      <input type="text" />
+      <button>add</button>
+      <ul>
+        <li>list item</li>
+      </ul>
+      `,    
 })
+
 export class AppComponent {
-  title = 'app works!';
-}
+  
+  public items: string[];
+
+  constructor(){
+    this.items = [];
+  }
+
+  public addItem(title:string){
+    this.items.push(title);
+  }
+
+  public removeItem(item: string) {
+    const index = this.items.indexOf(item); 
+    this.items.splice(index, 1);
+  }
+} 
