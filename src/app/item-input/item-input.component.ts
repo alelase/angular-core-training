06d3baa3e83a6a5f64957386737b5b0d0e66087d
nir@500tech.com <nir@500tech.com>
2016-12-01 15:26:18 +0200

    updates

--- src/app/item-input/item-input.component.ts
+++ src/app/item-input/item-input.component.ts
@@ -8,15 +8,13 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
           [placeholder]="text" />
           
       <button (click)="itemClick.emit(itemInput.value)" 
-            (contextmenu)="showMenu($event)">add</button>
-   `  
+              (contextmenu)="showMenu($event)">add</button>
+   `
 })
 
-export class ItemInputComponent  {
+export class ItemInputComponent {
 
-  @Input() 
-  public text: string; 
+  @Input() public text: string;
+  @Output() public itemClick = new EventEmitter();
 
-  @Output()
-  public itemClick = new EventEmitter();  
 }
